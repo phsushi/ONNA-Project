f9da712ca295fed4178499a5f8c2dd65eaa955e7
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-10-14 15:30:44 -0300

    Acréscimo de duas procedures do tipo get

--- api/onna-api/src/routes/artigo.ts
+++ api/onna-api/src/routes/artigo.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 
-import { exibir_Artigo_Menstruacao, exibir_Artigo_Engravidar, exibir_Artigo_Gestacao, exibir_Artigo_Maternidade, exibir_Artigo_Cuidados_Corpo } from "../controllers/artigo"
+import { exibir_Artigo_Menstruacao, exibir_Artigo_Engravidar, exibir_Artigo_Gestacao, exibir_Artigo_Maternidade, exibir_Artigo_Cuidados_Corpo, consulta_Titulo_Artigo, consulta_Artigo_Selecionado } from "../controllers/artigo"
 
 const artigoRoutes:Router = Router()
 
@@ -9,5 +9,7 @@ artigoRoutes.get("/artigo/engravidar", exibir_Artigo_Engravidar)
 artigoRoutes.get("/artigo/gestacao", exibir_Artigo_Gestacao)
 artigoRoutes.get("/artigo/maternidade", exibir_Artigo_Maternidade)
 artigoRoutes.get("/artigo/cuidadosCorpo", exibir_Artigo_Cuidados_Corpo)
+artigoRoutes.get("/artigo/tituloArtigo", consulta_Titulo_Artigo)
+artigoRoutes.get("/artigo/artigoSelecionado", consulta_Artigo_Selecionado)
 
 export default artigoRoutes
