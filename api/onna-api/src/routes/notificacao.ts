42753406c1b720e515a9a58f99bffc7938b7a767
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-10-14 15:20:59 -0300

    Remoção procedure [alertar_Notificacao]

--- api/onna-api/src/routes/notificacao.ts
+++ api/onna-api/src/routes/notificacao.ts
@@ -1,13 +1,12 @@
 import { Router } from "express"
 
-import { criar_Notificacao, exibir_Lista_Notificacoes, excluir_Notificacao, alertar_Notificacao } from "../controllers/notificacao"
+import { criar_Notificacao, exibir_Lista_Notificacoes, excluir_Notificacao } from "../controllers/notificacao"
 
 const notificacaoRoutes:Router = Router()  
 
 notificacaoRoutes.post("/notificacao/:idUsuario", criar_Notificacao)
 
 notificacaoRoutes.get("/eNotificacao/:idUsuario", exibir_Lista_Notificacoes)
-notificacaoRoutes.get("/aNotificacao/:idUsuario", alertar_Notificacao) // checar
 
 notificacaoRoutes.delete("/notificacao/:idNotificacao", excluir_Notificacao)
 
