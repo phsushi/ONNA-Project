013c6074937475ad54fa29d05a59f57dd5541347
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-10-14 15:18:59 -0300

    Remoção procedure [armazena_Forum]

--- api/onna-api/src/routes/forum.ts
+++ api/onna-api/src/routes/forum.ts
@@ -1,11 +1,9 @@
 import { Router } from "express"
 
-import { criar_Forum, armazena_idForum } from "../controllers/forum"
+import { criar_Forum } from "../controllers/forum"
 
 const forumRoutes:Router = Router()
 
 forumRoutes.post("/forum", criar_Forum)
-
-forumRoutes.get("/forum", armazena_idForum)
 
 export default forumRoutes
