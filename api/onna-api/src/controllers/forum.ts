34a962742d35893d166bd22147a035ce8a0417e5
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-10-14 15:19:58 -0300

    Remoção procedure [armazena_Forum]

--- api/onna-api/src/controllers/forum.ts
+++ api/onna-api/src/controllers/forum.ts
@@ -19,26 +19,3 @@ export const criar_Forum = async (req:Request, res:Response) => {
         return res.status(200).json(values)
     })
 }
-
-// armazena_idForum
-export const armazena_idForum = async (req:Request, res:Response) => {
-    const q = "call armazena_idForum(?)"
-
-    const values = [
-        req.body.nome
-    ]
-
-    db.query(q, [values], (err, data) => {
-        if(err){
-            return res.status(500).json(err)
-        }
-        
-        if (data[0].length === 0) {
-            return res.status(404).json(err);
-        }
-
-        const idForum = data[0][0]
-
-        return res.status(200).json(idForum)
-    })
-}
