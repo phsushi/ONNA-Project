58ea8e1080e96c764a4d43fb4c6548b2e30d8bf4
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-10-28 20:04:38 -0300

    Update postagem.ts
    - Adição [exibir_Postagem_Especifica]  
    - Adição [exibir_Postagem_Popular]   
    - Adição [exibir_Postagem_Geral]       
    - Adição [exibir_Postagem_Propria]   
    - Adição [contagem_Postagem]     
    - Adição [alterar_Postagem]              
    - Adição [excluir_Postagem]

--- api/onna-api/src/routes/postagem.ts
+++ api/onna-api/src/routes/postagem.ts
@@ -1,16 +1,22 @@
 import { Router } from "express"
 
-import { criar_Postagem, exibir_Postagem, consulta_Titulo_Postagem, gostei_Postagem, desgostei_Postagem } from "../controllers/postagem"
+import { criar_Postagem, exibir_Postagem_Especifica, exibir_Postagem_Popular, exibir_Postagem_Forum, consulta_Titulo_Postagem, exibir_Postagem_Geral, exibir_Postagem_Propria, contagem_Postagem, alterar_Postagem, excluir_Postagem, gostei_Postagem, desgostei_Postagem } from "../controllers/postagem"
 
 const postagemRoutes:Router = Router()
 
 postagemRoutes.post("/postagem", criar_Postagem)
 
-postagemRoutes.get("/postagem/:idForum", exibir_Postagem)
+postagemRoutes.get("/postagem/:idPostagem", exibir_Postagem_Especifica)
+postagemRoutes.get("/postagem", exibir_Postagem_Popular)
+postagemRoutes.get("/postagem/:idForum", exibir_Postagem_Forum)
 postagemRoutes.get("/postagem/:titulo/:idForum", consulta_Titulo_Postagem)
+postagemRoutes.get("/postagem", exibir_Postagem_Geral)
+postagemRoutes.get("/postagem", exibir_Postagem_Propria)
+postagemRoutes.get("/postagem", contagem_Postagem)
 
+postagemRoutes.put("/postagem", alterar_Postagem)
+postagemRoutes.put("/postagem", excluir_Postagem)
 postagemRoutes.put("/postagemG", gostei_Postagem)
 postagemRoutes.put("/postagemD", desgostei_Postagem)
-
 
 export default postagemRoutes
