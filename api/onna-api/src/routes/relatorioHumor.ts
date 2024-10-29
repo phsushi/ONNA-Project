c34b343dc84437e0652d120c7899ef67e69960f9
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-10-29 12:47:09 -0300

    Update relatorioHumor.ts
    - Alteração [exibir_Relatorio]

--- api/onna-api/src/routes/relatorioHumor.ts
+++ api/onna-api/src/routes/relatorioHumor.ts
@@ -1,11 +1,11 @@
 import { Router } from "express"
 
-import { criar_Relatorio_Humor, exibir_Relatorios} from "../controllers/relatorioHumor"
+import { criar_Relatorio_Humor, exibir_Relatorio} from "../controllers/relatorioHumor"
 
 const relatorioHumorRoutes:Router = Router()  
 
 relatorioHumorRoutes.post("/relatorioHumor", criar_Relatorio_Humor)
 
-relatorioHumorRoutes.get("/relatorioHumor/:idUsuario", exibir_Relatorios)
+relatorioHumorRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio)
 
 export default relatorioHumorRoutes
