df46af578d2871667575ca3aa01dd474e6308662
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-05 11:11:12 -0300

    Update relatorio.ts

---
+++ api/onna-api/src/routes/relatorio.ts
@@ -0,0 +1,15 @@
+import { Router } from "express"
+
+import { criar_Relatorio_Humor, criar_Relatorio_Remedio, criar_Relatorio_Atividade, exibir_Relatorio_Humor, exibir_Relatorio_Remedio, exibir_Relatorio_Atividade} from "../controllers/relatorio"
+
+const relatorioRoutes:Router = Router()  
+
+relatorioRoutes.post("/relatorioHumor", criar_Relatorio_Humor)
+relatorioRoutes.post("/relatorioHumor", criar_Relatorio_Remedio)
+relatorioRoutes.post("/relatorioHumor", criar_Relatorio_Atividade)
+
+relatorioRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio_Humor)
+relatorioRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio_Humorxibir_Relatorio_Remedio)
+relatorioRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio_Atividade)
+
+export default relatorioRoutes
