1db9608804383af7441b87cbbcee00b107821458
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-12 14:19:14 -0300

    Atualizar o relatorio.ts

--- api/onna-api/src/routes/relatorio.ts
+++ api/onna-api/src/routes/relatorio.ts
@@ -4,12 +4,12 @@ import { criar_Relatorio_Humor, criar_Relatorio_Remedio, criar_Relatorio_Ativida
 
 const relatorioRoutes:Router = Router()  
 
-relatorioRoutes.post("/relatorioHumor", criar_Relatorio_Humor)
-relatorioRoutes.post("/relatorioHumor", criar_Relatorio_Remedio)
-relatorioRoutes.post("/relatorioHumor", criar_Relatorio_Atividade)
+relatorioRoutes.post("/relatorioH", criar_Relatorio_Humor)
+relatorioRoutes.post("/relatorioR", criar_Relatorio_Remedio)
+relatorioRoutes.post("/relatorioA", criar_Relatorio_Atividade)
 
-relatorioRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio_Humor)
-relatorioRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio_Remedio)
-relatorioRoutes.get("/relatorioHumor/:idUsuario/:data_Relatorio", exibir_Relatorio_Atividade)
+relatorioRoutes.get("/relatorioH/:idUsuario/:data_Relatorio", exibir_Relatorio_Humor)
+relatorioRoutes.get("/relatorioR/:idUsuario/:data_Relatorio", exibir_Relatorio_Remedio)
+relatorioRoutes.get("/relatorioA/:idUsuario/:data_Relatorio", exibir_Relatorio_Atividade)
 
 export default relatorioRoutes
