12976596fa2514cbdc598c8d1a7e2f304c0d0d21
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-13 18:25:45 -0300

    Atualizar o artigo.ts

--- api/onna-api/src/routes/artigo.ts
+++ api/onna-api/src/routes/artigo.ts
@@ -11,6 +11,6 @@ artigoRoutes.get("/artigo/maternidade", exibir_Artigo_Maternidade)
 artigoRoutes.get("/artigo/cuidadosCorpo", exibir_Artigo_Cuidados_Corpo)
 
 artigoRoutes.get("/artigo/:titulo", consulta_Titulo_Artigo)
-artigoRoutes.get("/artigo/geral", exibir_Artigos)
+artigoRoutes.get("/artigo", exibir_Artigos)
 
 export default artigoRoutes
