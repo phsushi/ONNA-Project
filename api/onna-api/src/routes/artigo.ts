8e8175bff21b31c0c0a83d2738979df424271c4b
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-15 18:13:25 -0300

    Update artigo.ts

--- api/onna-api/src/routes/artigo.ts
+++ api/onna-api/src/routes/artigo.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 
-import { exibir_Artigo_Menstruacao, exibir_Artigo_Engravidar, exibir_Artigo_Gestacao, exibir_Artigo_Maternidade, exibir_Artigo_Cuidados_Corpo, consulta_Titulo_Artigo, exibir_Artigos } from "../controllers/artigo"
+import { exibir_Artigo_Menstruacao, exibir_Artigo_Engravidar, exibir_Artigo_Gestacao, exibir_Artigo_Maternidade, exibir_Artigo_Cuidados_Corpo, artigo_Selecionado, consulta_Titulo_Artigo, exibir_Artigos } from "../controllers/artigo"
 
 const artigoRoutes:Router = Router()
 
@@ -9,6 +9,7 @@ artigoRoutes.get("/artigo/engravidar", exibir_Artigo_Engravidar)
 artigoRoutes.get("/artigo/gestacao", exibir_Artigo_Gestacao)
 artigoRoutes.get("/artigo/maternidade", exibir_Artigo_Maternidade)
 artigoRoutes.get("/artigo/cuidadosCorpo", exibir_Artigo_Cuidados_Corpo)
+artigoRoutes.get("/artigo/selecionado/:idArtigo", artigo_Selecionado)
 
 artigoRoutes.get("/artigo/:titulo", consulta_Titulo_Artigo)
 artigoRoutes.get("/artigo", exibir_Artigos)
