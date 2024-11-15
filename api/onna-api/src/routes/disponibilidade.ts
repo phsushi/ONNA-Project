c6d90cd0e62ca0b7568dacb47fb87d09e06f7bd4
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-15 12:23:08 -0300

    Update disponibilidade.ts

--- api/onna-api/src/routes/disponibilidade.ts
+++ api/onna-api/src/routes/disponibilidade.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"
 
-import { criar_Disponibilidade, exibir_Disponibilidade, alterar_Disponibilidade, excluir_Disponibilidade } from "../controllers/disponibilidade"
+import { criar_Disponibilidade, exibir_Disponibilidade, exibir_Pro_Disponivel, alterar_Disponibilidade, excluir_Disponibilidade } from "../controllers/disponibilidade"
 
 const disponibilidadeRoutes:Router = Router()  
 
 disponibilidadeRoutes.post("/disponibilidade", criar_Disponibilidade)
 
 disponibilidadeRoutes.get("/disponibilidade/:idProfissional", exibir_Disponibilidade)
+disponibilidadeRoutes.get("/disponibilidadePD/:data_Atual", exibir_Pro_Disponivel)
 
 disponibilidadeRoutes.put("/disponibilidade", alterar_Disponibilidade)
 
