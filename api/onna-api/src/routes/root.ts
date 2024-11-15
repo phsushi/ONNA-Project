bb15827e725dc97f9d64b1d252087cbfe324807b
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-15 11:16:09 -0300

    Update root.ts

--- api/onna-api/src/routes/root.ts
+++ api/onna-api/src/routes/root.ts
@@ -3,13 +3,9 @@ import { Router } from "express"
 import adminRoutes from "./admin"
 import artigoRoutes from "./artigo"
 import calendarioPersonalizadoRoutes from "./calendarioPersonalizado"
-import colecaoContatoRoutes from "./colecaoContato"
-import contatoRoutes from "./contato"
-import conversaPrivadaRoutes from "./conversaPrivada"
 import denunciaRoutes from "./denuncia"
 import disponibilidadeRoutes from "./disponibilidade"
 import loginRoutes from "./login"
-import notificacaoRoutes from "./notificacao"
 import perfisRoutes from "./perfis"
 import postagemRoutes from "./postagem"
 import relatorioRoutes from "./relatorio"
@@ -21,13 +17,9 @@ const rootRouter:Router = Router()
 rootRouter.use(adminRoutes)
 rootRouter.use(artigoRoutes)
 rootRouter.use(calendarioPersonalizadoRoutes)
-rootRouter.use(colecaoContatoRoutes)
-rootRouter.use(conversaPrivadaRoutes)
-rootRouter.use(contatoRoutes)
 rootRouter.use(denunciaRoutes)
 rootRouter.use(disponibilidadeRoutes)
 rootRouter.use(loginRoutes)
-rootRouter.use(notificacaoRoutes)
 rootRouter.use(perfisRoutes)
 rootRouter.use(postagemRoutes)
 rootRouter.use(relatorioRoutes)
