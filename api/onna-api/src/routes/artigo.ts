f0cb60377ff5debc0edd858887bca9c6b4b6b001
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-17 13:24:21 -0300

    Update artigo.ts

--- api/onna-api/src/routes/artigo.ts
+++ api/onna-api/src/routes/artigo.ts
@@ -4,9 +4,9 @@ import { exibir_Artigo_Genero, artigo_Selecionado, consulta_Titulo_Artigo, exibi
 
 const artigoRoutes:Router = Router()
 
-artigoRoutes.get("/artigo/genero/:idGenero", exibir_Artigo_Genero)
-artigoRoutes.get("/artigo/selecionado/:idArtigo", artigo_Selecionado)
-artigoRoutes.get("/artigo/:titulo", consulta_Titulo_Artigo)
+artigoRoutes.get("/artigoG/:idGenero", exibir_Artigo_Genero)
+artigoRoutes.get("/artigoS/:idArtigo", artigo_Selecionado)
+artigoRoutes.get("/artigoT/:titulo", consulta_Titulo_Artigo)
 artigoRoutes.get("/artigo", exibir_Artigos)
 
 export default artigoRoutes
