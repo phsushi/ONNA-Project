78fafb6ff74022704b2949a6c9682fc248a776fc
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-17 13:22:56 -0300

    Update artigo.ts

--- api/onna-api/src/controllers/artigo.ts
+++ api/onna-api/src/controllers/artigo.ts
@@ -2,83 +2,19 @@ import { Request, Response } from "express"
 
 import { db } from "../db/connection"
 
-// exibir_Artigo_Menstruacao
-export const exibir_Artigo_Menstruacao = async (req:Request, res:Response) => {
-    const q = "call exibir_Artigo_Menstruacao()"
+// exibir_Artigo_Genero
+export const exibir_Artigo_Genero = async (req:Request, res:Response) => {
+    const q = "call exibir_Artigo_Genero(?)"
 
-    db.query(q, (err, data) => {
+    const values = [
+        req.params.idGenero
+    ]
+
+    db.query(q, [values], (err, data) => {
         if(err){
             return res.status(500).json(err)
         }
-
-        if (data[0].length === 0) {
-            return res.status(404).json(err);
-        }
-
-        return res.status(200).json(data[0])
-    })
-}
-
-// exibir_Artigo_Engravidar
-export const exibir_Artigo_Engravidar = async (req:Request, res:Response) => {
-    const q = "call exibir_Artigo_Engravidar()"
-
-    db.query(q, (err, data) => {
-        if(err){
-            return res.status(500).json(err)
-        }
-
-        if (data[0].length === 0) {
-            return res.status(404).json(err);
-        }
-
-        return res.status(200).json(data[0])
-    })
-}
-
-// exibir_Artigo_Gestacao
-export const exibir_Artigo_Gestacao = async (req:Request, res:Response) => {
-    const q = "call exibir_Artigo_Gestacao()"
-
-    db.query(q, (err, data) => {
-        if(err){
-            return res.status(500).json(err)
-        }
-
-        if (data[0].length === 0) {
-            return res.status(404).json(err);
-        }
-
-        return res.status(200).json(data[0])
-    })
-}
-
-// exibir_Artigo_Maternidade
-export const exibir_Artigo_Maternidade = async (req:Request, res:Response) => {
-    const q = "call exibir_Artigo_Maternidade()"
-
-    db.query(q, (err, data) => {
-        if(err){
-            return res.status(500).json(err)
-        }
-
-        if (data[0].length === 0) {
-            return res.status(404).json(err);
-        }
-
-        return res.status(200).json(data[0])
-    })
-}
-
-// exibir_Artigo_Cuidados_Corpo
-export const exibir_Artigo_Cuidados_Corpo = async (req:Request, res:Response) => {
-    const q = "call exibir_Artigo_Cuidados_Corpo()"
-
-    db.query(q, (err, data) => {
-        if(err){
-            return res.status(500).json(err)
-        }
-
+        
         if (data[0].length === 0) {
             return res.status(404).json(err);
         }
