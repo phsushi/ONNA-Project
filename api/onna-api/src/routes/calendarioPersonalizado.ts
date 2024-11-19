f0c39f3f903f9f305541df0dd592d7e4e6c7c251
Roberto Brito <162512109+RobsGabs@users.noreply.github.com>
2024-11-19 19:08:54 -0300

    Update calendarioPersonalizado.ts

--- api/onna-api/src/routes/calendarioPersonalizado.ts
+++ api/onna-api/src/routes/calendarioPersonalizado.ts
@@ -1,13 +1,15 @@
 import { Router } from "express"
 
-import { cadastrar_Calendario, alterar_Calendario, definir_Intervalo_Calendario } from "../controllers/calendarioPersonalizado"
+import { cadastrar_Calendario,  exibir_Calendario, alterar_Calendario, excluir_Calendario } from "../controllers/calendarioPersonalizado"
 
 const calendarioPersonalizadoRoutes:Router = Router()  
 
 calendarioPersonalizadoRoutes.post("/calendarioPersonalizado", cadastrar_Calendario)
 
-calendarioPersonalizadoRoutes.get("/calendarioPersonalizado/:idUsuario", definir_Intervalo_Calendario)
+calendarioPersonalizadoRoutes.get("/calendarioPersonalizado/:idUsuario", exibir_Calendario)
 
 calendarioPersonalizadoRoutes.put("/calendarioPersonalizado", alterar_Calendario)
+
+calendarioPersonalizadoRoutes.delete("/calendarioPersonalizado", excluir_Calendario)
 
 export default calendarioPersonalizadoRoutes
